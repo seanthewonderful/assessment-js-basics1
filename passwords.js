d3b896943707e4b0015ad82cecce6c7553f6dbee
Sean <seanthewonderful@gmail.com>
2022-02-11 12:07:19 -0700

    password minrequirement commit

--- passwords.js
+++ passwords.js
@@ -6,8 +6,11 @@ const reader = readline.createInterface({
     output: process.stdout,
 })
 
-reader.question("Please enter a password for me to... validate", function(input){
-    inputArr = input.split(` `)
-    console.log(inputArr)
-    console.log(inputArr[1])
+reader.question("Please enter a password for me to... validate  ", function(input){
+//   console.log(input.length)
+    if(input.length>=10){
+        console.log("Well done, you may continue...")
+    }else if(input.length<10){
+        console.log("Oh no, your password is too short for me. Let's one with at least 10 characters next time.")
+    }
 })
